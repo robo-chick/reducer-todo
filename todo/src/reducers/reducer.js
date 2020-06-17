bef949c9a64131ba3e749de31f4b91c69400025c
Tasha <tashingsworth@gmail.com>
2020-06-17 13:00:07 -0400

    Initial commit. Built reducer.

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,47 @@
+export const initialState = {
+    todos: [
+        {
+            item: "work on todo project",
+            completed: false,
+            id: 1
+        },
+        {
+            item: "go to the gym",
+            completed: false,
+            id: 2
+        }
+    ]
+}
+
+export const reducer = (state, action) => {
+    console.log(action);
+    switch(action.type) {
+        case "ADD_TODO":
+            return {
+                ...state,
+                todos: [...state.todos, action.payload]
+            }
+
+        case "TOGGLE_COMPLETED":
+            return {
+                ...state,
+                todos: state.todos.map((e) => {
+                    if (e.id === action.payload) {
+                        return {
+
+                            ...e,
+                            completed: !e.completed
+                        }
+                    } else {
+                        return e;
+                    }
+                })
+            }
+
+        case "CLEAR_COMPLETED":
+            return {
+                ...state,
+                todos: state.todos.filter(item => !item.completed)
+            }
+    }
+}
